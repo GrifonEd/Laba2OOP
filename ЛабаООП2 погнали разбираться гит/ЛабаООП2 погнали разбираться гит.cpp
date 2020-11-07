1200529b1e2b79a7414987e378fe4399bc6fe8d5
Edik Daukaev <daukaev.eduard@mail.ru>
2020-11-07 21:47:22 +0500

    Изменение 2

--- ЛабаООП2 погнали разбираться гит/ЛабаООП2 погнали разбираться гит.cpp
+++ ЛабаООП2 погнали разбираться гит/ЛабаООП2 погнали разбираться гит.cpp
@@ -1,13 +1,39 @@
 ﻿
-
+#include <conio.h>
 #include <iostream>
 using namespace std;
+
+class Animal { //Создание класса
+public :
+    int size, age;           //Создание атрибутов(публичных)
+    Animal() {                        // Создание Конструктора без параметров 
+        printf("Animal()\n");
+        size = 0;
+        age = 0;
+    }
+    Animal(int size, int age) {            // Создание Конструктора с параметрами
+        printf("Animal(int size, int age)\n");
+        this->size = size;
+        this->age = age;
+    }
+    Animal(const Animal &a) {                  // Создание Конструктора копирования
+        printf("Animal(const Animal &a)\n");
+        size = a.size;
+        age = a.age;
+    }
+    ~Animal() {                      // Создание деструктора
+        printf("%d, %d\n",size, age);
+        printf("~Animal()\n");
+    }
+};
 int main()
 {
-    int n;
-    cin >> n;
-    cout << n*2;
-    cout << "Hello World!\n";
+    {
+    Animal a;
+    Animal a2(10, 20);                   // Создание объектов класса
+    Animal a3(a2);
+    }
+ 
 }
 
 
