5ccea1d691a7bc739d9567e8bd919699d4022f14
Edik Daukaev <daukaev.eduard@mail.ru>
2020-11-07 22:23:03 +0500

    Изменение 4

--- ЛабаООП2 погнали разбираться гит/ЛабаООП2 погнали разбираться гит.cpp
+++ ЛабаООП2 погнали разбираться гит/ЛабаООП2 погнали разбираться гит.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 class Animal { //Создание класса
+private:
+    int character=0;
 public :
     int size, age;           //Создание атрибутов(публичных)
     Animal() {                        // Создание Конструктора без параметров 
@@ -22,19 +24,33 @@ public :
         age = a.age;
     }
     ~Animal() {                      // Создание деструктора
-        printf("%d, %d\n",size, age);
+        printf("%d, %d, %d\n",size, age, character);
         printf("~Animal()\n");
     }
+    void train(int time, int strength) {
+        size = size + time * strength;
+        age = age + time * strength;
+    }
+    void recreation();
 };
+void Animal::recreation() {
+    size = size - 1;
+    age = age - 1;
+    character = character + 1;
+}
 int main()
 {
+    setlocale(LC_CTYPE, "Russian");
     {
+        printf("Создание объектов статически\n");
     Animal a;
     Animal a2(10, 20);                   // Создание объектов класса
     Animal a3(a2);
     }
-    
+    system("pause");
+    system("cls");
     {
+        printf("Создание объектов динамически\n");
         Animal *a = new Animal;
         Animal *a2 = new Animal(10, 20);
         Animal* a3 = new Animal(*a2);
@@ -43,8 +59,20 @@ int main()
         delete a2;
         delete a3;
     }
-
-
+    system("pause");
+    system("cls");
+    {
+        printf("Работа методов и доступа к атрибутам\n");
+        Animal* a = new Animal(10, 20);
+        a->train(2, 3);
+        a->recreation();                   // Добавляем к атрибуту "сharecter" 1 через метод даже к приватному.
+        a->age = 0; // Можем обратиться к возрасту, потому что атрибут "age" публичный
+        // a->character не можем обратиться, потому что атрибут "сharecter" приватный
+        delete a;
+        
+    }
+    system("pause");
+    system("cls");
 
 
  
